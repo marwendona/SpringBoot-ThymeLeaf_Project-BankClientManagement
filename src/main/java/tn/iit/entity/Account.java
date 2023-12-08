fe4d499fd399be0f6657381c5d5310a497e43860
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-08 14:50:40 +0100

    add Account

---
+++ src/main/java/tn/iit/entity/Account.java
@@ -0,0 +1,35 @@
+package tn.iit.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "account")
+public class Account implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer rib;
+
+    @Column(name = "balance")
+    private float balance;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    @ToString.Exclude
+    private Client client;
+}
