2f12bc62c3502d66dd74e4e92f9eb8de52723468
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-11 00:07:26 +0100

    update clients.js

--- src/main/resources/static/js/clients.js
+++ src/main/resources/static/js/clients.js
@@ -1,42 +1,26 @@
 function deleteClient(cin) {
-    Swal.fire({
-        title: 'Are you sure?',
-        text: 'Once deleted, you will not be able to recover this client!',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Yes, delete it!',
-        cancelButtonText: 'No, keep it'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                url: '/clients/delete-ajax',
-                type: 'POST',
-                data: { cin: cin },
-                success: function () {
-                    Swal.fire({
-                        title: 'Deleted!',
-                        text: 'Your client has been deleted.',
-                        icon: 'success',
-                        timer: 1500,
-                        onClose: () => {
-                            location.reload();
-                        }
-                    });
-                },
-                error: function () {
-                    Swal.fire({
-                        title: 'Error!',
-                        text: 'Failed to delete the client.',
-                        icon: 'error'
-                    });
-                }
-            });
-        } else {
-            Swal.fire(
-                'Cancelled',
-                'Your client is safe :)',
-                'error'
-            );
-        }
-    });
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this client!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                $.ajax({
+                    url: "/clients/delete-ajax",
+                    type: "POST",
+                    data: {'cin': cin},
+                    success: function () {
+                        $("#" + cin).remove();
+                        swal("Poof! Your client has been deleted!", {
+                            icon: "success",
+                        });
+                    }
+                });
+            } else {
+                swal("Your client is safe!");
+            }
+        });
 }
