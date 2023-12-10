03c314cd31af38f4540b168a8fd8fc6a6e4f808e
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-10 16:42:41 +0100

    update AccountController

--- src/main/java/tn/iit/controller/AccountController.java
+++ src/main/java/tn/iit/controller/AccountController.java
@@ -1,60 +1,63 @@
 package tn.iit.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import tn.iit.dto.AccountDto;
-import tn.iit.entity.Account;
 import tn.iit.service.AccountService;
 
-import java.util.List;
-
-@RestController
+@Controller
 @RequestMapping("/accounts")
-@CrossOrigin
 public class AccountController {
     private final AccountService accountService;
 
-    @Autowired
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
     }
 
-    @PostMapping("/{cin}")
-    public ResponseEntity<Long> createAccount(@PathVariable Long cin, @RequestBody AccountDto accountDto) {
-        Long newRib = accountService.createAccount(accountDto, cin);
-        return ResponseEntity.ok(newRib);
-    }
-
-    @GetMapping("/{rib}")
-    public ResponseEntity<Account> getAccountByRib(@PathVariable Long rib) {
-        Account account = accountService.getAccountByRib(rib);
-        if (account != null) {
-            return ResponseEntity.ok(account);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
     @GetMapping
-    public ResponseEntity<List<Account>> getAllAccounts() {
-        List<Account> accounts = accountService.getAllAccounts();
-        return ResponseEntity.ok(accounts);
+    public ModelAndView getAllAccounts() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("accounts", accountService.getAllAccounts());
+        modelAndView.setViewName("accounts");
+        return modelAndView;
     }
 
-    @PutMapping("/{rib}")
-    public ResponseEntity<Long> updateAccount(@PathVariable Long rib, @RequestBody AccountDto updatedAccountDto) {
-        Long updatedRib = accountService.updateAccount(rib, updatedAccountDto);
-        if (updatedRib != null) {
-            return ResponseEntity.ok(updatedRib);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    @PostMapping
+    public String createAccount(@RequestParam(name = "rib") Long rib,
+                                @RequestParam(name = "balance") float balance,
+                                @RequestParam(name = "cin") Long cin) {
+        AccountDto accountDto = AccountDto.builder()
+                .rib(rib)
+                .balance(balance)
+                .build();
+        accountService.createAccount(accountDto, cin);
+        return "redirect:/accounts";
     }
 
-    @DeleteMapping("/{rib}")
-    public ResponseEntity<Void> deleteAccount(@PathVariable Long rib) {
+    @GetMapping("/add")
+    public String showAddAccountForm(Model model) {
+        model.addAttribute("account", new AccountDto());
+        return "add-account";
+    }
+
+    @GetMapping("/delete/{rib}")
+    public String deleteAccount(@PathVariable(name = "rib") Long rib) {
         accountService.deleteAccount(rib);
-        return ResponseEntity.noContent().build();
+        return "redirect:/accounts";
+    }
+
+    @GetMapping("/edit/{rib}")
+    public String editAccount(@PathVariable Long rib, Model model) {
+        AccountDto accountDto = accountService.getAccountByRib(rib);
+        model.addAttribute("account", accountDto);
+        return "edit-account";
+    }
+
+    @PostMapping("/{rib}")
+    public String updateAccount(@PathVariable(name = "rib") Long rib, @ModelAttribute AccountDto updatedAccountDto) {
+        accountService.updateAccount(rib, updatedAccountDto);
+        return "redirect:/accounts";
     }
 }
